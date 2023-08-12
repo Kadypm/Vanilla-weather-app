1386f0cba83e6103be197abb1c6219c1d97a7018
FWebMom <fkperezm@gmail.com>
2023-08-12 21:18:49 +0200

    trying to fix js

--- src/app.js
+++ src/app.js
@@ -1,12 +1,32 @@
-function displayTemperature(response){
-    console.log(response.data.main.temp);
-    let temperatureElement =document.querySelector("#temperature");
-    let cityElement = document.querySelector("#city");
-    temperatureElement.innerHTML = Math.round(response.data.main.temp);
-        cityElement.innerHTML = response.data.name;
+function formatDate(timestamp){
+let date = new Date (timestamp);
+let hours = date.getHouts();
+let minutes =  date.getMinutes();
+let day = date.getDay();
+return `${day}  ${hours}:${minutes}`;
 }
 
-let apiKey = "56b9a37d4289fb4202o351ecd010dta9";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Lisbon&key=56b9a37d4289fb4202o351ecd010dta9&units=metric`;
 
-axios.get(apiUrl).then(displayTemperature);
+function showTemperature(response) {
+    console.log(response);
+
+    let cityElement = document.querySelector("#city");
+    let temperatureElement = document.querySelector ("#temperature");
+    let descriptionElement = document.querySelector("#description");
+    //let sensationElementElement = document.querySelector("#precipitation");
+    let humidityElement = document.querySelector("#humidity");
+
+
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    cityElement.innerHTML = (response.data.name);
+    descriptionElement.innerHTML = (response.data.weather[0].description);
+    //sensationElement.innerHTML = response. data.main.feels_like;
+    humidityElement.innerHTML = (response.data.main.humidity);
+    dateElement.innerHTML = formatDate(response.data.dt * 1000);
+   
+}
+
+let apiKey = "eb508ef6233a171a1bdb79ec0ccb8c7a";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+axios.get(`${apiUrl}`).then(showTemperature);;
+
