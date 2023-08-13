be6fc03c1f85dd2debac85078e405db8e3e5b649
FWebMom <fkperezm@gmail.com>
2023-08-13 21:53:11 +0200

    forecast doesnt work

--- src/app.js
+++ src/app.js
@@ -80,7 +80,7 @@ function displayForecast(response){
 
 function getForecast(coordinates) {
     let apiKey = "6a8552e0d268bfe64b16546e8fbb0a9a";
-    let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
 
     axios.get`(${apiUrl})`.then(displayForecast);
 }
